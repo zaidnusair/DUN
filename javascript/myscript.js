54934b04a23cfaf0a982b834b15f2d1deedb2f88
zaid nusair <zaid.nusair@hotmail.com>
2020-05-01 19:17:18 +0300

    added js

---
+++ javascript/myscript.js
@@ -0,0 +1,14 @@
+`use strict` 
+
+alert('Hello! Welcome ')
+
+
+
+function myFunction() {
+    var person = prompt("Please enter your name", "Harry Potter");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hello " + person + "! How are you today?";
+    }
+  }
+
