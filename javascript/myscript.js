c9bdb2db3cbd458ab6210b77bf47404c875d324c
zaid nusair <zaid.nusair@hotmail.com>
2020-05-03 11:56:47 +0300

    added prompt and functions

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,14 +1,64 @@
-`use strict` 
+`use strict`
 
 alert('Hello! Welcome ')
 
 
 
 function myFunction() {
-    var person = prompt("Please enter your name", "Harry Potter");
-    if (person != null) {
-      document.getElementById("demo").innerHTML =
+  var person = prompt("Please enter your name", "Harry Potter");
+  if (person !== null) {
+    document.getElementById("demo").innerHTML =
       "Hello " + person + "! How are you today?";
-    }
   }
+}
+
+function myFunction1() {
+  var ask = prompt('do you have an account?');
+  if (ask == 'yes') {
+
+    var r = confirm("good");
+  }
+
+
+
+
+  else if (ask == 'no') {
+    
+    return myFunction2();
+    function myFunction2(){
+      want = prompt('would you like to have one?');
+    
+      
+      
+      while (want == 'no'){
+        return myFunction2();}
+          if (want == 'yes') {
+            var r = confirm("good");
+            }
+        }
+        
+      }
+    }
+  
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
